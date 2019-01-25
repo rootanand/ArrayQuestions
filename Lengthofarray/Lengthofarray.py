582528e3831344209b70e76533caf99f26ada1f5
Udhaikumar Mohan <udhaikumar4@gmail.com>
2019-01-25 15:44:06 +0530

    length of array

---
+++ Lengthofarray/Lengthofarray.py
@@ -0,0 +1,28 @@
+# Find the length of the given array:
+
+def lengthofarray(A):
+
+    count=0
+
+    while(A[count:]):
+        count+=1
+
+    return count
+
+#Getting Input from user:
+
+#N=input("Enter the Number of elements: ")
+
+#A=[]
+
+#for a in range (0,N):
+    
+	#X = input("Enter the Numbers: ")
+    
+	#A.append(X)
+
+#print A
+
+
+#print("The length of the given array is: ")
+#print(lengthofarray(A))
