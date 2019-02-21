f24858e8849dfd11e2aea73bc687b412a9378aaf
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-21 23:31:55 +0530

    Code Revised Completely

---
+++ removeDuplicatesonly/findLenght.py
@@ -0,0 +1,18 @@
+Question :
+==========
+    To find the lenght of an array using slicing methodogy
+Input :
+=======
+    Unsorted Integer Array 
+Output:
+=======
+    Count the No of Element in the Array - Integer
+Code:
+=====
+def FindLenght(array):
+    count = 0
+    while(a[count:]):
+        count += 1    
+    return count
+
+ 
